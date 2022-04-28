1384f997f6e39208bbb91899550077d3c99b391c
Mikołaj Pawełek <Pajfot@gmail.com>
2022-04-28 22:19:22 +0200

    Utworzenie publicznej klasy CatFact wraz z konstruktorem

--- CatFacts/Program.cs
+++ CatFacts/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace CatFacts
 {
+
+    public class CatFact
+    {
+        public CatFact(string url)
+        {
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+            using var webResponse = request.GetResponse();
+            using var webStream = webResponse.GetResponseStream();
+
+            using var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+            Console.WriteLine(data);
+
+        }
+
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new CatFact("https://catfact.ninja/fact");
         }
     }
 }
